14f607159200ca72e172f3da42414bfe38987311
Wells Riley <wells.riley@gmail.com>
2016-05-01 00:44:04 -0700

    tracker

--- README.md
+++ README.md
@@ -1,47 +1,34 @@
-![](https://raw.githubusercontent.com/wr/figma-mac/master/extras/Example.png)
+# Pivotal Tracker for Mac (unofficial)
 
-# Figma for Mac (unofficial)
-
-This is an [Electron](https://github.com/atom/electron) wrapper for [Figma](https://figma.com). It lets you install Figma on your Mac and easily access it from the Dock instead of your web browser.
+This is an [Electron](https://github.com/atom/electron) wrapper for Pivotal Tracker. It lets you install Tracker on your Mac and easily access it from the Dock instead of your web browser.
 
 ## Caveats
 
 I'm not really a Node developer, so this is just a basic implementation. There are some known (and some unknown!) issues with this project:
 
-- The Mac stoplights overlap the top navigation because I've chosen to use the `hidden-inset` `title-bar-style`. I've injected some CSS to fix it on launch, but it's buggy.
-- Pinch to zoom does not work. I imagine this is solvable, but I don't know how.
-- Files open in a new window :[
-- This is a third-party project not associated with Figma, Inc., so use it at your own risk.
+- This is a third-party project not associated with Pivotal Labs, so use it at your own risk.
 
 ## How to Install
 
-1. Download the latest [Release](https://github.com/wr/figma-mac/releases/) from GitHub, unzip, and drag to your Applications folder.
+1. Download the latest [Release](https://github.com/wr/tracker-mac/releases/) from GitHub, unzip, and drag to your Applications folder.
 
 2. Launch the app.
 
-3. Log in with your Figma credentials.
+3. Log in with your Tracker credentials.
 
 4. Party hardy.
-
-## I need help!
-
-This project is far from finished. I created it because I prefer desktop apps over webapps, and I wanted to give Figma a try. So far, I think it shows tons of promise.
-
-If you have experience with Node and / or Electron, and would like to help make this app cleaner, leaner, faster, and more reliable with me, please submit a PR or email me at wells.riley@me.com with "Figma for Mac" somewhere in the subject line. Should be pretty fun!
-
-![](http://wellsosaur.us/dzqG/figma-icon.png)
 
 ## Troubleshooting
 
 ¯\_(ツ)_/¯
 
-Figma is still in beta, and so is this Mac app. Stuff will probably break. Feel free to [submit a GitHub issue](https://github.com/wr/figma-mac/issues) or [contact the official Figma support line](https://www.figma.com/contact) if you need help.
+This Mac app is a hack. Stuff will probably break. Feel free to [submit a GitHub issue](https://github.com/wr/tracker-mac/issues) if you need help.
 
 ## Building it yourself
 
 1. Download [Electron](https://github.com/atom/electron) via NPM (`electron-prebuilt`)
-2. `cd` to this repo on your Mac, make whatever changes you like, and run `electron .` to run a live preview of your Figma app.
-3. To package the app as a binary, download [electron-packager](https://github.com/maxogden/electron-packager) and run it from the directory above the repo:
+2. `cd` to this repo on your Mac, make whatever changes you like, and run `electron .` to run a live preview of your app.
+3. To package the app as a binary, download [electron-packager](https://github.com/maxogden/electron-packager) and run it from this directory:
 ```
-electron-packager figma Figma --platform=darwin --arch=x64 --version=0.35.2 --app-version=[your-version] --icon='figma/extras/atom.icns'
+npm run mac
 ```
--- main.js
+++ main.js
@@ -1,6 +1,150 @@
 const electron = require('electron');
-const app = electron.app;  // Module to control application life.
-const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
+const app = electron.app;
+const globalShortcut = electron.globalShortcut;
+const session = require('electron').session;
+var BrowserWindow = require('browser-window');
+var Menu = require('menu');
+
+var force_quit = false;
+
+var menu = Menu.buildFromTemplate([
+  {
+    label: 'Pivotal Tracker',
+    submenu: [
+      {
+        label: 'About Pivotal Tracker',
+        selector: 'orderFrontStandardAboutPanel:'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Hide Pivotal Tracker',
+        accelerator: 'CmdOrCtrl+H',
+        click: function() {mainWindow.hide();}
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Quit Pivotal Tracker',
+        accelerator: 'CmdOrCtrl+Q',
+        click: function() {force_quit=true; app.quit();}
+      },
+    ]
+  },
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Save',
+        accelerator: 'CmdOrCtrl+S'
+      }
+    ]
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      {
+        label: 'Undo',
+        accelerator: 'CmdOrCtrl+Z',
+        role: 'undo'
+      },
+      {
+        label: 'Redo',
+        accelerator: 'Shift+CmdOrCtrl+Z',
+        role: 'redo'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Cut',
+        accelerator: 'CmdOrCtrl+X',
+        role: 'cut'
+      },
+      {
+        label: 'Copy',
+        accelerator: 'CmdOrCtrl+C',
+        role: 'copy'
+      },
+      {
+        label: 'Paste',
+        accelerator: 'CmdOrCtrl+V',
+        role: 'paste'
+      },
+      {
+        label: 'Select All',
+        accelerator: 'CmdOrCtrl+A',
+        role: 'selectall'
+      },
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      {
+        label: 'Reload',
+        accelerator: 'CmdOrCtrl+R',
+        click: function() {mainWindow.reload();}
+      },
+      {
+        label: 'Toggle Sidebar',
+        accelerator: 'Shift+Cmd+L',
+        click: function() {toggleSidebar();}
+      },
+      {
+        label: 'Toggle Full Screen',
+        accelerator: (function() {
+          if (process.platform == 'darwin')
+            return 'Ctrl+Command+F';
+          else
+            return 'F11';
+        })(),
+        click: function(item, focusedWindow) {
+          if (focusedWindow)
+            focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+        }
+      },
+      {
+        label: 'Toggle Developer Tools',
+        accelerator: (function() {
+          if (process.platform == 'darwin')
+            return 'Alt+Command+I';
+          else
+            return 'Ctrl+Shift+I';
+        })(),
+        click: function() {mainWindow.toggleDevTools();}
+      },
+    ]
+  },
+  {
+    label: 'Window',
+    role: 'window',
+    submenu: [
+      {
+        label: 'Minimize',
+        accelerator: 'CmdOrCtrl+M',
+        role: 'minimize'
+      },
+      {
+        label: 'Close',
+        accelerator: 'CmdOrCtrl+W',
+        role: 'close'
+      },
+    ]
+  },
+  {
+    label: 'Help',
+    role: 'help',
+    submenu: [
+      {
+        label: 'Pivotal Tracker on GitHub',
+        click: function() { require('electron').shell.openExternal('https://github.com/wr/tracker-mac') }
+      },
+    ]
+  }
+]);
 
 // Report crashes to our server.
 //electron.crashReporter.start();
@@ -21,32 +165,47 @@ app.on('window-all-closed', function() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
+  Menu.setApplicationMenu(menu);
+
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1600, height: 1100, 'node-integration': false, 'title-bar-style': 'hidden-inset'});
+  mainWindow = new BrowserWindow({
+    width: 2000,
+    height: 1200,
+    minWidth: 900,
+    minHeight: 550,
+    'node-integration': false,
+    //'title-bar-style': 'hidden-inset',
+    'web-preferences': {'web-security': false},
+    zoomFactor: 1.3
+  });
 
   // and load the index.html of the app.
-  mainWindow.loadURL('https://figma.com/login');
+  mainWindow.loadURL('https://www.pivotaltracker.com/n/projects/1528533');
 
   // open _blank links in same window
   mainWindow.webContents.on('new-window', function(e, url) {
     e.preventDefault();
-    mainWindow.loadURL(url);
+    require('shell').openExternal(url);
   });
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools();
 
-  // Inject some useful CSS
-  mainWindow.show();
-  mainWindow.webContents.on('did-finish-load', function() {
-    mainWindow.webContents.insertCSS(".logo {width: 130px !important; margin-left: 70px !important}")
+  mainWindow.on('close', function(e){
+    if(!force_quit){
+      e.preventDefault();
+      mainWindow.hide();
+    }
   });
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
+  mainWindow.on('closed', function(){
     mainWindow = null;
+    globalShortcut.unregisterAll()
+    app.quit();
   });
+
+  app.on('activate', function(){
+    mainWindow.show();
+  });
+
 });
